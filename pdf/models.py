1889b866a85f3c87659ea20e54d93cc593791147
DCarbo-D42 <kareemmagdy85888@gmail.com>
2025-01-06 11:27:54 +0200

    adding pdffile model to pdf app models.py

--- pdf/models.py
+++ pdf/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class PDFFile(models.Model):
+    file = models.FileField(upload_to='uploads/pdfs/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    number_of_pages = models.PositiveIntegerField(null=True, blank=True)
+    page_width = models.PositiveIntegerField(null=True, blank=True)
+    page_height = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"PDF: {self.file.name}"
