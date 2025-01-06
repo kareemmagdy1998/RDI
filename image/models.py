1773c519a73a700fd308db712b6353eca5bd9bd7
DCarbo-D42 <kareemmagdy85888@gmail.com>
2025-01-06 11:25:31 +0200

    adding imagefile model to image app models.py

--- image/models.py
+++ image/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ImageFile(models.Model):
+    file = models.ImageField(upload_to='uploads/images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    width = models.PositiveIntegerField(null=True, blank=True)
+    height = models.PositiveIntegerField(null=True, blank=True)
+    channels = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Image: {self.file.name}"
